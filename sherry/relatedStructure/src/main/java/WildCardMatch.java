e42a4532e18ca109eb6da8d500ed76829a1983bc
Liyuan Ma <liyuanma@ca.ibm.com>
2018-08-28 09:51:39 -0300

    Week 9: working on Word related questions

--- sherry/relatedStructure/src/main/java/WildCardMatch.java
+++ sherry/relatedStructure/src/main/java/WildCardMatch.java
@@ -54,5 +54,63 @@ public class WildCardMatch {
 		return true;
 	}
 
+	public boolean isMatchRegular(String s, String p) {
+		if (s == null || p == null) {
+			return false;
+		}
 
+		boolean[][] memo = new boolean[s.length()][p.length()];
+		boolean[][] visited = new boolean[s.length()][p.length()];
+
+		return isMatchHelper(s, 0, p, 0, memo, visited);
+	}
+
+	private boolean isMatchHelperRegular(String s, int sIndex,
+								  String p, int pIndex,
+								  boolean[][] memo,
+								  boolean[][] visited) {
+		// "" == ""
+		if (pIndex == p.length()) {
+			return sIndex == s.length();
+		}
+
+		if (sIndex == s.length()) {
+			return isEmpty(p, pIndex);
+		}
+
+		if (visited[sIndex][pIndex]) {
+			return memo[sIndex][pIndex];
+		}
+
+		char sChar = s.charAt(sIndex);
+		char pChar = p.charAt(pIndex);
+		boolean match;
+
+		// consider a* as a bundle
+		if (pIndex + 1 < p.length() && p.charAt(pIndex + 1) == '*') {
+			match = isMatchHelperRegular(s, sIndex, p, pIndex + 2, memo, visited) ||
+					charMatchRegular(sChar, pChar) && isMatchHelperRegular(s, sIndex + 1, p, pIndex, memo, visited);
+		} else {
+			match = charMatchRegular(sChar, pChar) &&
+					isMatchHelperRegular(s, sIndex + 1, p, pIndex + 1, memo, visited);
+		}
+
+		visited[sIndex][pIndex] = true;
+		memo[sIndex][pIndex] = match;
+		return match;
+	}
+
+	private boolean charMatchRegular(char sChar, char pChar) {
+		return sChar == pChar || pChar == '.';
+	}
+
+	private boolean isEmpty(String p, int pIndex) {
+		for (int i = pIndex; i < p.length(); i += 2) {
+			if (i + 1 >= p.length() || p.charAt(i + 1) != '*') {
+				return false;
+			}
+		}
+		return true;
+	}
 }
+
